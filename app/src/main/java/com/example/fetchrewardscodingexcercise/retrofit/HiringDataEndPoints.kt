efdbb779a473c316b8b49cb99bff7602b58188af
BumbleFlash <sudarshan97@gmail.com>
2021-03-05 15:56:34 -0500

    chore: added retrofit modules

---
+++ app/src/main/java/com/example/fetchrewardscodingexcercise/retrofit/HiringDataEndPoints.kt
@@ -0,0 +1,11 @@
+package com.example.fetchrewardscodingexcercise.retrofit
+
+import com.example.fetchrewardscodingexcercise.model.HiringData
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface HiringDataEndPoints {
+
+    @GET("hiring.json")
+    fun getHiringData(): Call<HiringData>
+}
---
+++ app/src/main/java/com/example/fetchrewardscodingexcercise/retrofit/RetrofitServiceBuilder.kt
@@ -0,0 +1,22 @@
+package com.example.fetchrewardscodingexcercise.retrofit
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitServiceBuilder {
+
+    private const val BASE_URL = "https://fetch-hiring.s3.amazonaws.com/"
+
+    private val okHttpClient = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
+        .build()
+
+    fun <T> buildService(service: Class<T>): T {
+        return retrofit.create(service)
+    }
+}
