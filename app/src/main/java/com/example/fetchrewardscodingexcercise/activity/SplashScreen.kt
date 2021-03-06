d049bd7a2b9584d62c6df82078697059173460eb
BumbleFlash <sudarshan97@gmail.com>
2021-03-06 13:32:31 -0500

    chore: added networkUtils to SplashScreen.kt and created an alert if network is not available

--- app/src/main/java/com/example/fetchrewardscodingexcercise/activity/SplashScreen.kt
+++ app/src/main/java/com/example/fetchrewardscodingexcercise/activity/SplashScreen.kt
@@ -1,11 +1,15 @@
 package com.example.fetchrewardscodingexcercise.activity
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fetchrewardscodingexcercise.R
+import com.example.fetchrewardscodingexcercise.utils.NetworkUtils
+import kotlin.system.exitProcess
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +22,20 @@ class SplashScreen : AppCompatActivity() {
         )
 
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+            if (NetworkUtils.isNetworkAvailable(applicationContext)) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                AlertDialog.Builder(this@SplashScreen)
+                        .setTitle(resources.getString(R.string.no_network))
+                        .setMessage(resources.getString(R.string.no_network_summary))
+                        .setPositiveButton(android.R.string.ok) { _: DialogInterface, _: Int ->
+                            exitProcess(1)
+                        }
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
         }, 3000)
     }
 }
