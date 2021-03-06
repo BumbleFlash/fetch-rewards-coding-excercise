a617713b962a292d56bf03847dcd0d6ba95ad6ef
BumbleFlash <sudarshan97@gmail.com>
2021-03-05 22:33:21 -0500

    chore: added Progress bar

--- app/src/main/java/com/example/fetchrewardscodingexcercise/activity/MainActivity.kt
+++ app/src/main/java/com/example/fetchrewardscodingexcercise/activity/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.fetchrewardscodingexcercise.activity
 
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fetchrewardscodingexcercise.R
@@ -20,11 +25,23 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
     private var recyclerView: RecyclerView? = null
     private var dataCount: TextView? = null
+    private var progressBar: ProgressBar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView = findViewById(R.id.recyclerView)
         dataCount = findViewById(R.id.data_count)
+        progressBar = findViewById(R.id.progress)
+        progressBar?.visibility = View.VISIBLE
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            progressBar?.indeterminateDrawable
+                ?.setColorFilter(
+                    ContextCompat.getColor(this, R.color.fr_yellow),
+                    PorterDuff.Mode.SRC_IN
+                );
+        }
+
         val request = RetrofitServiceBuilder.buildService(HiringDataEndPoints::class.java)
         val call = request.getHiringData()
 
@@ -54,6 +71,7 @@ class MainActivity : AppCompatActivity() {
         dataCount?.text = String.format(
             resources.getString(R.string.data_count), filteredAndSortedData.size
         )
+        progressBar?.visibility = View.GONE
         recyclerView?.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@MainActivity)
