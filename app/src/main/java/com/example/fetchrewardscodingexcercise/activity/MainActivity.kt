87c9ce17a6f835ac60a71fb6fd449ea31a93b8bd
Sudarshan <sudarshan97@gmail.com>
2021-03-05 22:36:17 -0500

    feat: beautify app (#11)
    * chore: changed theme to match that of Fetch Rewards
    
    * chore: changed theme to match that of Fetch Rewards
    
    * chore: Added a SplashScreen Activity
    
    * chore: added Progress bar

--- app/src/main/java/com/example/fetchrewardscodingexcercise/activity/MainActivity.kt
+++ app/src/main/java/com/example/fetchrewardscodingexcercise/activity/MainActivity.kt
@@ -1,11 +1,18 @@
-package com.example.fetchrewardscodingexcercise
+package com.example.fetchrewardscodingexcercise.activity
 
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.fetchrewardscodingexcercise.R
 import com.example.fetchrewardscodingexcercise.adapter.HiringDataAdapter
 import com.example.fetchrewardscodingexcercise.model.HiringData
 import com.example.fetchrewardscodingexcercise.retrofit.HiringDataEndPoints
@@ -17,10 +24,24 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private var recyclerView: RecyclerView? = null
+    private var dataCount: TextView? = null
+    private var progressBar: ProgressBar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView = findViewById(R.id.recyclerView)
+        dataCount = findViewById(R.id.data_count)
+        progressBar = findViewById(R.id.progress)
+        progressBar?.visibility = View.VISIBLE
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            progressBar?.indeterminateDrawable
+                ?.setColorFilter(
+                    ContextCompat.getColor(this, R.color.fr_yellow),
+                    PorterDuff.Mode.SRC_IN
+                );
+        }
+
         val request = RetrofitServiceBuilder.buildService(HiringDataEndPoints::class.java)
         val call = request.getHiringData()
 
@@ -47,6 +68,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setData(hiringDataList: List<HiringData>) {
         val filteredAndSortedData = HiringDataUtils.filterAndSortData(hiringDataList)
+        dataCount?.text = String.format(
+            resources.getString(R.string.data_count), filteredAndSortedData.size
+        )
+        progressBar?.visibility = View.GONE
         recyclerView?.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@MainActivity)
